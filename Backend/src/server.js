00ee18515c0c9d95f65621deb18508b251551a73
NavMani <navjot10b@gmail.com>
2026-08-17 05:50:41 +0530

    Update for depeloyment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,18 +3,36 @@ import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 
-app.use(cors()); // Enable CORS for all routes
+if(process.env.NODE_ENV !== "proeduction"){
+    app.use(cors(
+    {
+        origin: "http://localhost:5173",
+    }
+));
+} // Enable CORS for all routes
+
+
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(rateLimiter); // Apply rate limiting middleware to all routes
 
 app.use("/api/notes", notesRoutes);
+
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"../Frontend", "dist", "index.html"))
+});
+}
 
 
 connectDB().then(() => {
